ec80f8f6f2d44c54e2e49a50be7dd34c0295c529
Abhijeet Shashwat <abhijeet.s.ug23@plaksha.edu.in>
2024-06-26 21:11:03 +0530

    navigation

--- app/src/main/java/com/example/rstm/MainActivity.kt
+++ app/src/main/java/com/example/rstm/MainActivity.kt
@@ -11,37 +11,45 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.rstm.ui.theme.RSTMTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
             RSTMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "RS & TM Project",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    NavHost(navController = navController, startDestination = "home") {
+                        composable("home") {
+                            Text(
+                                text = "Home",
+                                modifier = Modifier.padding(innerPadding)
+                            )
+                        }
+                        composable("details") {
+                            Text(
+                                text = "Details",
+                                modifier = Modifier.padding(innerPadding)
+                            )
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
+    val navController = rememberNavController()
     RSTMTheme {
-        Greeting("Android")
+
     }
 }
