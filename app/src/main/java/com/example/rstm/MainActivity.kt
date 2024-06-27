e22d67b6475ef559caefe7c7a063c0d286556a8d
Abhijeet Shashwat <abhijeet.s.ug23@plaksha.edu.in>
2024-06-27 15:15:02 +0530

    logic of accelerometer complete

--- app/src/main/java/com/example/rstm/MainActivity.kt
+++ app/src/main/java/com/example/rstm/MainActivity.kt
@@ -1,42 +1,51 @@
 package com.example.rstm
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.rstm.ui.screens.AccelerometerScreen
+import com.example.rstm.ui.screens.HomeScreen
+import com.example.rstm.ui.screens.accActivity
 import com.example.rstm.ui.theme.RSTMTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+
+
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
+
+           setContent {
             val navController = rememberNavController()
             RSTMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     NavHost(navController = navController, startDestination = "home") {
                         composable("home") {
-                            Text(
-                                text = "Home",
-                                modifier = Modifier.padding(innerPadding)
-                            )
+                            HomeScreen(Modifier.padding(innerPadding))
                         }
-                        composable("details") {
-                            Text(
-                                text = "Details",
-                                modifier = Modifier.padding(innerPadding)
-                            )
+                        composable("accelerometer"){
+                            AccelerometerScreen(Modifier.padding(innerPadding))
                         }
                     }
                 }
@@ -45,11 +54,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    val navController = rememberNavController()
+fun DefaultPreview() {
     RSTMTheme {
-
+        HomeScreen(Modifier.padding(16.dp))
     }
 }
---
+++ app/src/main/java/com/example/rstm/ui/screens/homeScreen.kt
@@ -0,0 +1,27 @@
+package com.example.rstm.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(modifier: Modifier) {
+    Column(modifier){
+        val navController = rememberNavController()
+        TopAppBar(title = { Text(text = "RSTM"    )})
+        Button(onClick = {
+            accActivity()
+            navController.navigate("accelerometer")
+                         },) {
+            Text(text = "Accelerometer Screen")
+        }
+    }
+
+}
