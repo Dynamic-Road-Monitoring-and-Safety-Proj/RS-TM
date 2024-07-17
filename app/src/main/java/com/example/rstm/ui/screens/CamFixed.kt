918ff87663ad4ad4576ad176da318471d0122025
Abhijeet Shashwat <abhijeet.s.ug23@plaksha.edu.in>
2024-07-17 21:50:35 +0530

    merging fixed external with my logic

---
+++ app/src/main/java/com/example/rstm/ui/screens/CamFixed.kt
@@ -0,0 +1,134 @@
+package com.example.rstm.ui.screens
+
+package com.app.cmrxtutorial.composables
+
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.Context
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.Quality
+import androidx.camera.video.QualitySelector
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoCapture
+import androidx.camera.video.VideoRecordEvent
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.Executor
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+import androidx.core.util.Consumer
+import androidx.camera.video.MediaStoreOutputOptions
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+
+@Composable
+fun CameraPreviewScreen() {
+    val lensFacing = CameraSelector.LENS_FACING_BACK
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val context = LocalContext.current
+    val cameraxSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+
+    val recBuilder = Recorder.Builder()
+    val qualitySelector = QualitySelector.fromOrderedList(
+        listOf(Quality.FHD, Quality.HD, Quality.HIGHEST)
+    )
+    val recorder = recBuilder.setQualitySelector(qualitySelector).build()
+    val videoCapture: VideoCapture<Recorder> = VideoCapture.withOutput(recorder)
+    lateinit var recording:Recording
+    LaunchedEffect(lensFacing) {
+        val cameraProvider = context.getCameraProvider()
+        cameraProvider.unbindAll()
+        cameraProvider.bindToLifecycle(lifecycleOwner, cameraxSelector, videoCapture)
+    }
+    Box(contentAlignment = Alignment.BottomCenter, modifier = Modifier.fillMaxSize()) {
+        Button(onClick = {
+            recording = captureVideo(videoCapture, context, ContextCompat.getMainExecutor(context))
+            CoroutineScope(Dispatchers.Main).launch {
+                delay(20000)
+                recording.stop()
+                Toast.makeText(context,"done",LENGTH_SHORT).show()
+            }
+        }
+        ){
+            Text(text = "Capture Video 20 sec")
+        }
+    }
+}
+
+@SuppressLint("MissingPermission")
+private fun captureVideo(
+    videoCapture: VideoCapture<Recorder>,
+    context: Context,
+    executor: Executor
+): Recording {
+    val name = "CameraX-recording-" +
+            SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+                .format(System.currentTimeMillis()) + ".mp4"
+    val contentValues = ContentValues().apply {
+        put(MediaStore.Video.Media.DISPLAY_NAME, name)
+    }
+    val mediaStoreOutput = MediaStoreOutputOptions.Builder(context.contentResolver,
+        MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
+        .setContentValues(contentValues)
+        .build()
+
+    val captureListener = Consumer<VideoRecordEvent> { event ->
+        when (event) {
+            is VideoRecordEvent.Start -> {
+                Log.d("CameraScreen", "Recording started")
+            }
+            is VideoRecordEvent.Finalize -> {
+                if (event.error == VideoRecordEvent.Finalize.ERROR_NONE) {
+                    Log.d("CameraScreen", "Video recording succeeded: ${event.outputResults.outputUri}")
+                } else {
+                    Log.e("CameraScreen", "Video recording failed: ${event.cause}")
+                }
+            }
+            else -> {
+                // Handle other events if needed
+            }
+        }
+    }
+
+    return videoCapture.output
+        .prepareRecording(context, mediaStoreOutput)
+        .withAudioEnabled()
+        .start(executor, captureListener)
+}
+
+private suspend fun Context.getCameraProvider(): ProcessCameraProvider =
+    suspendCoroutine { continuation ->
+        ProcessCameraProvider.getInstance(this).also { cameraProvider ->
+            cameraProvider.addListener({
+                continuation.resume(cameraProvider.get())
+            }, ContextCompat.getMainExecutor(this))
+        }
+    }
