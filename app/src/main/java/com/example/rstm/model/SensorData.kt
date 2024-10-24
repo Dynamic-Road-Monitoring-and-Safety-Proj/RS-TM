4501a387245c135fcaa102e3fa86d9c41804a5f9
Abhijeet Shashwat <asp616848@gmail.com>
2024-10-24 11:24:38 +0530

    Update SensorData.kt

--- app/src/main/java/com/example/rstm/model/SensorData.kt
+++ app/src/main/java/com/example/rstm/model/SensorData.kt
@@ -2,9 +2,12 @@ package com.example.rstm.model
 
 import android.location.Location
 
+// Data class to store sensor data similar to a state class as this is the point of absolute truth for the collective VMs.
+
 data class SensorData(
-    val accelerometerData: MutableList<Pair<Long,Triple<Float, Float, Float>>> = mutableListOf(),  // timestamp, x, y, z
-    val gyroscopeData: MutableList<Pair<Long,Triple<Float, Float, Float>>> = mutableListOf(),
-    val lightData: MutableList<Pair<Long, Float>> = mutableListOf(),  // timestamp, light value
-    val locationData: MutableList<Pair<Long, Location>> = mutableListOf()
+    val accelerometerData: Triple<Float, Float, Float>,
+    val gyroscopeData: Triple<Float, Float, Float>,
+    val lightData:  Float,
+    val locationData: Location,
+    val timestamp : Long
 )
