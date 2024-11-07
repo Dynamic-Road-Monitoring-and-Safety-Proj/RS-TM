98d0088fc92719835d2def25d8a2fa0050625d59
Abhijeet Shashwat <asp616848@gmail.com>
2024-11-07 12:34:07 +0530

    Update RoomEntity.kt

--- app/src/main/java/com/example/rstm/roomImplementation/RoomEntity.kt
+++ app/src/main/java/com/example/rstm/roomImplementation/RoomEntity.kt
@@ -1,6 +1,5 @@
 package com.example.rstm.roomImplementation
 
-import android.net.Uri
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -8,6 +7,6 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "uri_table")
 data class RoomEntity(
     @PrimaryKey(autoGenerate = true) val id: Int,
-    @ColumnInfo(name = "video_uri_list") val videoUriList: List<Uri>?, // This should be a String
-    @ColumnInfo(name = "csv_uri") val csvUri: Uri?
+    @ColumnInfo(name = "video_uri_list") val videoUriList: List<String>?, // Changed to List<String>
+    @ColumnInfo(name = "csv_uri") val csvUri: String?  // Changed to String
 )
